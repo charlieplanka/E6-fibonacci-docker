997f8e634b3bf8244d10d9e834e11efd859cc598
Anastasia Naumenko <charlieplanka@gmail.com>
2021-04-06 19:52:27 +0300

    Configure simple Flask server

--- fib_numbers.py
+++ fib_numbers.py
@@ -1,16 +1,25 @@
 from pymemcache.client import base
+from flask import Flask
+from flask import request
+from flask import jsonify
 
+app = Flask(__name__)
 client = base.Client(("localhost", 11211))
 
 
-def get_fibo_num(k):
-    fibo = client.get(str(k))
+@app.route('/')
+def get_fibo_num():
+    num = int(request.args.get("k"))
+    if not num:
+        resp = jsonify(success=True)
+        resp.status_code = 200
+        return resp
+    
+    fibo = client.get(str(num))
     if fibo is None:
         fibo, fibo_next = 0, 1
-        for _ in range(k-1):
+        for _ in range(num-1):
             fibo, fibo_next = fibo_next, fibo + fibo_next
-        client.set(str(k), fibo)
-    return int(fibo)
-
-
-# print(get_fibo_num(1000))
+        client.set(str(num), fibo)
+    
+    return jsonify(fibo=int(fibo))
