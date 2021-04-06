2ac85983053f32e14832ca0d2bdbb32f04bbc057
Anastasia Naumenko <charlieplanka@gmail.com>
2021-04-06 19:33:58 +0300

    Add memcached

--- fib_numbers.py
+++ fib_numbers.py
@@ -1,5 +1,16 @@
+from pymemcache.client import base
+
+client = base.Client(("localhost", 11211))
+
+
 def get_fibo_num(k):
-    fibo_cur, fibo_next = 0, 1
-    for _ in range(k-1):
-        fibo_cur, fibo_next = fibo_next, fibo_cur + fibo_next
-    return fibo_cur
+    fibo = client.get(str(k))
+    if fibo is None:
+        fibo, fibo_next = 0, 1
+        for _ in range(k-1):
+            fibo, fibo_next = fibo_next, fibo + fibo_next
+        client.set(str(k), fibo)
+    return int(fibo)
+
+
+# print(get_fibo_num(1000))
