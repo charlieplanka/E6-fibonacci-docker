2b8c2696fa1a1b84ce21ca16445f68be76008382
Anastasia Naumenko <charlieplanka@gmail.com>
2021-04-13 12:26:02 +0300

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu
+ENV PORT 8081
+COPY ./requirements.txt /app/requirements.txt
+WORKDIR /app
+RUN pip install -r requirements.txt
+COPY ./app/app.py /app/app.py
+ENTRYPOINT ["python"]
+CMD ["app.py"]
--- app/app.py
+++ app/app.py
@@ -2,9 +2,11 @@ from pymemcache.client import base
 from flask import Flask
 from flask import request
 from flask import jsonify
+import os
 
 app = Flask(__name__)
 memcache_client = base.Client(("localhost", 11211))
+port = int(os.environ.get("PORT", 5000))
 
 
 @app.route('/<num>')
@@ -13,10 +15,14 @@ def get_fibo_num(num):
     if fibo is None:
         fibo = count_fibo_num(int(num))        
         memcache_client.set(num, fibo)
-    return jsonify(fibo=int(fibo))
+    return jsonify(fibo=int(fibo)), 200
 
 def count_fibo_num(num):
     fibo, fibo_next = 0, 1
     for _ in range(num - 1):
         fibo, fibo_next = fibo_next, fibo + fibo_next
     return fibo
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=port)
