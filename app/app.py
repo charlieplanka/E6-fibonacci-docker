4bd01c9baaa5ea11c5286ca8ce37c556a52a94fe
Anastasia Naumenko <charlieplanka@gmail.com>
2021-04-16 12:31:12 +0300

    Add docker-compose

--- app/app.py
+++ app/app.py
@@ -6,16 +6,15 @@ import os
 
 app = Flask(__name__)
 memcache_client = base.Client(("localhost", 11211))
-# port = int(os.environ.get("PORT", 5000))
 
 
 @app.route('/<num>')
 def get_fibo_num(num):
-    # fibo = memcache_client.get(num)
+    fibo = memcache_client.get(num)
     fibo = None
     if fibo is None:
         fibo = count_fibo_num(int(num))        
-        # memcache_client.set(num, fibo)
+        memcache_client.set(num, fibo)
     return jsonify(fibo=int(fibo)), 200
 
 def count_fibo_num(num):
@@ -23,7 +22,3 @@ def count_fibo_num(num):
     for _ in range(num - 1):
         fibo, fibo_next = fibo_next, fibo + fibo_next
     return fibo
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='127.0.0.1', port=port)
